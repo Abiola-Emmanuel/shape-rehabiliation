a6e3c9513dfb88fe184a89dc7f5afb1fbfe1284e
Abiola Emmanuel <sijuwolaabiola@gmail.com>
2026-01-15 13:35:05 +0100

    about page in progress

---
+++ about/about.js
@@ -0,0 +1,9 @@
+const menuDisplay = document.getElementById('menuDisplay')
+
+function openMenu() {
+  menuDisplay.classList.add('show');
+}
+
+function closeMenu() {
+  menuDisplay.classList.remove('show')
+}
