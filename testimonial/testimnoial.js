a3d8b530c79af476124db9c8d4962b63ceb25bb1
Abiola Emmanuel <sijuwolaabiola@gmail.com>
2026-01-16 09:11:48 +0100

    testimonial page

---
+++ testimonial/testimnoial.js
@@ -0,0 +1,20 @@
+AOS.init({
+  once: true
+});
+
+const menuDisplay = document.getElementById('menuDisplay')
+
+function home() {
+  window.location.href = '/';
+}
+
+
+function openMenu() {
+  menuDisplay.classList.add('show');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeMenu() {
+  menuDisplay.classList.remove('show');
+  document.body.style.overflow = '';
+}
